770b695d4eb25941174ddd63df776271745ca912
PranathiSadhula <pranathisadhula@gmail.com>
2023-01-21 21:30:43 +0530

    TestNG implementation to take screenshots on failure steps

---
+++ src/main/java/reporter/ScreenShotListener.java
@@ -0,0 +1,32 @@
+package reporter;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+
+import base.WebDriverImpl;
+import org.apache.commons.io.FileUtils;
+
+import org.openqa.selenium.*;
+
+import org.testng.*;
+import testExecutionEngine.ExecutionEngine;
+
+public class ScreenShotListener implements ITestListener {
+    @Override
+    public void onTestFailure(ITestResult result) {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat formater = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
+        String methodName = result.getName();
+        if(!result.isSuccess()){
+            File scrFile = ((TakesScreenshot) ExecutionEngine.driverImpl).getScreenshotAs(OutputType.FILE);
+            try {
+                String reportDirectory = new File(System.getProperty("user.dir")).getAbsolutePath() + "/target/surefire-reports";
+                File destFile = new File((String) reportDirectory+"/failure_screenshots/"+methodName+"_"+formater.format(calendar.getTime())+".png");
+                FileUtils.copyFile(scrFile, destFile);
+                Reporter.log("<a href='"+ destFile.getAbsolutePath() + "'> <img src='"+ destFile.getAbsolutePath() + "' height='100' width='100'/> </a>");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
